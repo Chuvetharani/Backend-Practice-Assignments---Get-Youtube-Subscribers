ba77ae9ac4deac2695b2640a0e97468083147535
Chuvetharani <68391363+Chuvetharani@users.noreply.github.com>
2020-12-13 01:10:30 +0530

    srabc

--- src/app.js
+++ src/app.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const app = express();
 const Subscriber = require('./models/subscribers');
-const ObjectId = require("mongoose");
 
 // Your code goes here
 app.get('/subscribers', (req, res) => {
     Subscriber.find().then(subscribers => res.send(subscribers));
-    return;
 });
 app.get('/subscribers/names', (req, res) => {
     Subscriber.find().select({name: 1, subscribedChannel: 1}).then(subscribers => res.send(subscribers));
