62f8759cbbc233da8770a58b5cf8b3e827691314
Chuvetharani <pmuchuvetha@gmail.com>
2020-12-13 22:33:06 +0530

    updates

--- src/app.js
+++ src/app.js
@@ -7,11 +7,20 @@ app.get('/subscribers', (req, res) => {
     Subscriber.find().then(subscribers => res.send(subscribers));
 });
 app.get('/subscribers/names', (req, res) => {
-    Subscriber.find().select({name: 1, subscribedChannel: 1}).then(subscribers => res.send(subscribers));
-    return;
+    let array = [];
+    Subscriber.find().then(ele => {
+        ele.map(data => {
+            let name = data.name;
+            let subscribedChannel = data.subscribedChannel;
+            array.push({name, subscribedChannel})
+        })
+        res.send(array)
+    })
 });
 app.get('/subscribers/:id', (req, res) => {
     const id = req.params.id;
     Subscriber.find({_id : id}).then(subscribers => subscribers.map(subscribers => res.send(subscribers))).catch(error => res.status(400).send({message: error.message}));
     return;
 });
+
+module.exports = app;
